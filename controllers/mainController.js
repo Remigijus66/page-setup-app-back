147d711edc6590cc11a1fbf0985ecc7031070ea3
Remigijus <you@example.com>
2022-11-03 12:56:58 +0200

    cleanup

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,15 +1,10 @@
-// const userSchema = require("../schemas/userAuthSchema");
-// const bcrypt = require("bcrypt");
-// const { uid } = require('uid');
 
-const userSettings = [{}]
+const userSettings = []
 
 module.exports = {
 
     loginSettings: (req, res) => {
         const { name } = req.body
-        console.log(userSettings)
-
         if (userSettings.filter(e => e.name === name).length > 0) {
             const i = userSettings.findIndex((e) => e.name === name)
             return res.send({ error: false, message: null, data: userSettings[i] });
@@ -19,8 +14,6 @@ module.exports = {
     },
     registerSettings: (req, res) => {
         const { name } = req.body
-        console.log(name)
-        console.log(userSettings)
         if (userSettings.filter(e => e.name === name).length > 0)
             return res.send({ error: true, message: "User alerady exists, please login", data: null });
         userSettings.push({ name: name })
@@ -28,7 +21,6 @@ module.exports = {
     },
     saveSettings: (req, res) => {
         const { name } = req.body
-        console.log('req', req.body)
         userSettings.forEach((e, i) => { if (e.name == name) userSettings[i] = req.body })
         console.log('usersettings', userSettings)
         return res.send({ error: false, message: "Settings saved", data: null })
@@ -39,7 +31,6 @@ module.exports = {
             return res.send({ error: false, message: "", data: null });
         userSettings.push(req.body)
 
-        console.log('usersettings', userSettings)
         return res.send({ error: false, message: "User registered", data: null })
     }
 }
